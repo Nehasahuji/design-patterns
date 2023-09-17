7f7e86cada01c130c46c49ad886146125af683fa
Neha Sahu <nehasahu9424@gmail.com>
2023-09-17 11:13:17 +0530

    Added Solution of assignment

--- src/main/java/com/scaler/lld/design/assignments/singleton/FileBasedConfigurationManagerImpl.java
+++ src/main/java/com/scaler/lld/design/assignments/singleton/FileBasedConfigurationManagerImpl.java
@@ -2,49 +2,84 @@ package com.scaler.lld.design.assignments.singleton;
 
 public class FileBasedConfigurationManagerImpl extends FileBasedConfigurationManager {
 
+    private static FileBasedConfigurationManagerImpl INSTANCE = null;
+
+    private FileBasedConfigurationManagerImpl(){
+        super();
+    }
+
     @Override
     public String getConfiguration(String key) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getConfiguration'");
+        if(INSTANCE!=null) {
+           return INSTANCE.getProperties().getProperty(key);
+        }else {
+            throw new NullPointerException("Configuration not exist for " + key);
+        }
     }
 
     @Override
     public <T> T getConfiguration(String key, Class<T> type) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getConfiguration'");
+        if(INSTANCE!=null) {
+            String str = INSTANCE.properties.getProperty(key);
+            if (str != null)
+                return convert(str, type);
+            else
+                return null;
+
+        }else{
+            throw new NullPointerException("Configuration not present for " + key + " and " + type);
+        }
     }
 
     @Override
     public void setConfiguration(String key, String value) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setConfiguration'");
+        if(INSTANCE!=null) {
+            INSTANCE.getProperties().setProperty(key,value);
+        }else{
+            throw new NullPointerException("Not able to set configuration for "+ key + " and " + value);
+        }
     }
 
     @Override
     public <T> void setConfiguration(String key, T value) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setConfiguration'");
+        if(INSTANCE!=null) {
+
+        }else{
+            throw new NullPointerException("Not able to set configuration for "+ key + " and " + value);
+        }
     }
 
     @Override
     public void removeConfiguration(String key) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeConfiguration'");
+        if(INSTANCE!=null) {
+            INSTANCE.getProperties().remove(key);
+        }else{
+            throw new NullPointerException("Not able to remove configuration for key " + key);
+        }
     }
 
     @Override
     public void clear() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'clear'");
+       if(INSTANCE!=null && INSTANCE.getProperties()!=null){
+            INSTANCE.getProperties().clear();
+       }else{
+           throw new NullPointerException("Not able to clear properties");
+       }
     }
 
     public static FileBasedConfigurationManager getInstance() {
-        // TODO Auto-generated method stub
-        return null;
+            if (INSTANCE == null) {
+                synchronized (FileBasedConfigurationManagerImpl.class) {
+                    if(INSTANCE==null) {
+                        INSTANCE = new FileBasedConfigurationManagerImpl();
+                    }
+                }
+            }
+        return INSTANCE;
     }
 
     public static void resetInstance() {
-        // TODO Auto-generated method stub
+        INSTANCE=null;
     }
 
 }
